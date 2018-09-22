501b33aff74d3f8fbab7fc9010d48e65c9b0f401
reepias <adthasit.coding@gmail.com>
2018-09-22 19:54:25 +0700

    use Firebase Cloud Messages

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import firebase from 'react-native-firebase'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  componentDidMount() {
+    this.initNotification()
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  initNotification = async () => {
+    await this.setPermission()
+    const fcmToken = await firebase.messaging().getToken()
+    console.log('fcmToken', fcmToken)
+  }
+
+  setPermission = async () => {
+    try {
+      const enabled = await firebase.messaging().hasPermission()
+      if (!enabled) {
+        await firebase.messaging().requestPermission()
+      }
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          Firebase Cloud Messaging 
+        </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -41,9 +56,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
